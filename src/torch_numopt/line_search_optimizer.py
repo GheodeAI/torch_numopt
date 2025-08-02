3442ee1e59e086361db46c8b0a4e0e2587190d83
eugenioLR <eugeniolorente0@gmail.com>
2025-08-02 16:55:32 +0200

    fixed interpolation

--- src/torch_numopt/line_search_optimizer.py
+++ src/torch_numopt/line_search_optimizer.py
@@ -124,54 +124,31 @@ class LineSearchOptimizer(CustomOptimizer, ABC):
 
     
     @torch.enable_grad()
-    def interpolate_quadratic(self, params, step_dir, grad, lr_init, eval_model, c1, c2, line_search_cond="armijo"):
-        dir_deriv = sum([torch.dot(p_grad.flatten(), p_step.flatten()) for p_grad, p_step in zip(grad, step_dir)])
-
-        loss = eval_model(*params)
-        lr = lr_init
-
-        new_params = tuple(p - lr * p_step for p, p_step in zip(params, step_dir))
-        new_loss = eval_model(*new_params)
-
-        print()
-        while not self.accept_step(params, new_params, step_dir, lr, loss, new_loss, grad, c1, c2, line_search_cond):
-            print(lr)
-            if lr == 0:
-                break
-
-            lr = - 0.5 * (dir_deriv * lr ** 2) / (new_loss - loss - dir_deriv * lr)
-
-            new_params = tuple(p - lr * p_step for p, p_step in zip(params, step_dir))
-            new_loss = eval_model(*new_params)
-
-        return new_params
-    
-    @torch.enable_grad()
     def interpolate_cubic(self, params, step_dir, grad, lr_init, eval_model, c1, c2, line_search_cond="armijo"):
         dir_deriv = sum([torch.dot(p_grad.flatten(), p_step.flatten()) for p_grad, p_step in zip(grad, step_dir)])
 
         loss = eval_model(*params)
-        lr_0 = lr_init
+        lr_0 = -lr_init
 
+        # Quadratic interpolation to obtain a new point
         # Calculate first interpolation point
-        prev_params = tuple(p - lr_0 * p_step for p, p_step in zip(params, step_dir))
+        prev_params = tuple(p + lr_0 * p_step for p, p_step in zip(params, step_dir))
         prev_loss = eval_model(*prev_params)
 
         if self.accept_step(params, prev_params, step_dir, lr_0, loss, prev_loss, grad, c1, c2, line_search_cond):
             return prev_params
         
-        # print()
         # Calculate second interpolation point
         lr_1 = - 0.5 * (dir_deriv * lr_0 ** 2) / (prev_loss - loss - dir_deriv * lr_0)
-        # print(lr_1)
 
-        new_params = tuple(p - lr_1 * p_step for p, p_step in zip(params, step_dir))
+        new_params = tuple(p + lr_1 * p_step for p, p_step in zip(params, step_dir))
         new_loss = eval_model(*new_params)
 
-        # print()
+        # Cubic interpolation with new calculated point
         while not self.accept_step(params, new_params, step_dir, lr_1, loss, new_loss, grad, c1, c2, line_search_cond):
             if lr_0 == 0 or lr_1 == 0 or lr_1 == lr_0:
                 break
+
             factor =  1 / ((lr_0 * lr_1)**2 * (lr_1 - lr_0))
             aux_mat = torch.Tensor([[lr_0**2, -lr_1**2], [-lr_0**3, lr_1**3]])
             aux_vec = torch.Tensor([new_loss - loss - dir_deriv*lr_1, prev_loss - loss - dir_deriv*lr_0])
@@ -179,75 +156,9 @@ class LineSearchOptimizer(CustomOptimizer, ABC):
             
             lr_0 = lr_1
             lr_1 = (-b + torch.sqrt(torch.abs(b**2 - 3*a*dir_deriv)))/(3*a)
-            # print(float(lr_0), float(lr_1), a, b, b**2 - 3*a*dir_deriv)
 
             prev_loss = new_loss
-            new_params = tuple(p - lr_1 * p_step for p, p_step in zip(params, step_dir))
-            new_loss = eval_model(*new_params)
-
-        return new_params
-
-    
-    @torch.enable_grad()
-    def interpolate_quadratic(self, params, step_dir, grad, lr_init, eval_model, c1, c2, line_search_cond="armijo"):
-        dir_deriv = sum([torch.dot(p_grad.flatten(), p_step.flatten()) for p_grad, p_step in zip(grad, step_dir)])
-
-        loss = eval_model(*params)
-        lr = lr_init
-
-        new_params = tuple(p - lr * p_step for p, p_step in zip(params, step_dir))
-        new_loss = eval_model(*new_params)
-
-        print()
-        while not self.accept_step(params, new_params, step_dir, lr, loss, new_loss, grad, c1, c2, line_search_cond):
-            print(lr)
-            if lr == 0:
-                break
-
-            lr = - 0.5 * (dir_deriv * lr ** 2) / (new_loss - loss - dir_deriv * lr)
-
-            new_params = tuple(p - lr * p_step for p, p_step in zip(params, step_dir))
-            new_loss = eval_model(*new_params)
-
-        return new_params
-    
-    @torch.enable_grad()
-    def interpolate_cubic(self, params, step_dir, grad, lr_init, eval_model, c1, c2, line_search_cond="armijo"):
-        dir_deriv = sum([torch.dot(p_grad.flatten(), p_step.flatten()) for p_grad, p_step in zip(grad, step_dir)])
-
-        loss = eval_model(*params)
-        lr_0 = lr_init
-
-        # Calculate first interpolation point
-        prev_params = tuple(p - lr_0 * p_step for p, p_step in zip(params, step_dir))
-        prev_loss = eval_model(*prev_params)
-
-        if self.accept_step(params, prev_params, step_dir, lr_0, loss, prev_loss, grad, c1, c2, line_search_cond):
-            return prev_params
-        
-        # print()
-        # Calculate second interpolation point
-        lr_1 = - 0.5 * (dir_deriv * lr_0 ** 2) / (prev_loss - loss - dir_deriv * lr_0)
-        # print(lr_1)
-
-        new_params = tuple(p - lr_1 * p_step for p, p_step in zip(params, step_dir))
-        new_loss = eval_model(*new_params)
-
-        # print()
-        while not self.accept_step(params, new_params, step_dir, lr_1, loss, new_loss, grad, c1, c2, line_search_cond):
-            if lr_0 == 0 or lr_1 == 0 or lr_1 == lr_0:
-                break
-            factor =  1 / ((lr_0 * lr_1)**2 * (lr_1 - lr_0))
-            aux_mat = torch.Tensor([[lr_0**2, -lr_1**2], [-lr_0**3, lr_1**3]])
-            aux_vec = torch.Tensor([new_loss - loss - dir_deriv*lr_1, prev_loss - loss - dir_deriv*lr_0])
-            a, b = factor.cpu()*torch.matmul(aux_mat, aux_vec)
-            
-            lr_0 = lr_1
-            lr_1 = (-b + torch.sqrt(torch.abs(b**2 - 3*a*dir_deriv)))/(3*a)
-            # print(float(lr_0), float(lr_1), a, b, b**2 - 3*a*dir_deriv)
-
-            prev_loss = new_loss
-            new_params = tuple(p - lr_1 * p_step for p, p_step in zip(params, step_dir))
+            new_params = tuple(p + lr_1 * p_step for p, p_step in zip(params, step_dir))
             new_loss = eval_model(*new_params)
 
         return new_params
@@ -273,9 +184,11 @@ class LineSearchOptimizer(CustomOptimizer, ABC):
             case "backtrack":
                 new_params = self.backtrack(params, step_dir, d_p_list, lr, eval_model, self.c1, self.c2, self.tau, self.line_search_cond)
             case "interpolate":
-                new_params =  self.interpolate_quadratic(params, step_dir, d_p_list, lr, eval_model, self.c1, self.c2, self.line_search_cond)
+                new_params =  self.interpolate_cubic(params, step_dir, d_p_list, lr, eval_model, self.c1, self.c2, self.line_search_cond)
             case "const":
                 new_params = tuple(p - lr * p_step for p, p_step in zip(params, step_dir))
+            case _:
+                raise ValueError("Incorrect line search method, try 'backtrack', 'interpolate' or 'const'.")
 
         # Apply new parameters
         for param, new_param in zip(params, new_params):
